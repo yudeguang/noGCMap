40e4e9ad94df831e4330b2be631ea2b396437faf
yudeguang <84558040@qq.com>
2022-09-05 22:04:00 +0800

    add any_benchmark_test

---
+++ any_benchmark_test.go
@@ -0,0 +1,371 @@
+package noGCMap
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/VictoriaMetrics/fastcache"
+	"sync"
+	"testing"
+	"unsafe"
+)
+
+func b2s(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+//noGCMap
+func BenchmarkSet(b *testing.B) {
+	const items = 1 << 16
+	c := New()
+	b.ReportAllocs()
+	b.SetBytes(items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		v := []byte("xyza")
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				c.Set(k, v)
+			}
+		}
+	})
+}
+
+//fastcache
+func BenchmarkFastcacheSet(b *testing.B) {
+	const items = 1 << 16
+	c := fastcache.New(12 * items)
+	defer c.Reset()
+	b.ReportAllocs()
+	b.SetBytes(items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		v := []byte("xyza")
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				c.Set(k, v)
+			}
+		}
+	})
+}
+
+//Std
+func BenchmarkStdMapSet(b *testing.B) {
+	const items = 1 << 16
+	m := make(map[string][]byte)
+	var mu sync.Mutex
+	b.ReportAllocs()
+	b.SetBytes(items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		v := []byte("xyza")
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				mu.Lock()
+				m[string(k)] = v
+				mu.Unlock()
+			}
+		}
+	})
+}
+
+//StdSyncMap
+func BenchmarkStdSyncMapSet(b *testing.B) {
+	const items = 1 << 16
+	m := sync.Map{}
+	b.ReportAllocs()
+	b.SetBytes(items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		v := "xyza"
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				m.Store(string(k), v)
+			}
+		}
+	})
+}
+
+//noGCMap
+func BenchmarkGet(b *testing.B) {
+	const items = 1 << 16
+	c := New()
+	k := []byte("\x00\x00\x00\x00")
+	v := []byte("xyza")
+	for i := 0; i < items; i++ {
+		k[0]++
+		if k[0] == 0 {
+			k[1]++
+		}
+		c.Set(k, v)
+	}
+	b.ReportAllocs()
+	b.SetBytes(items)
+	b.RunParallel(func(pb *testing.PB) {
+		var buf []byte
+		k := []byte("\x00\x00\x00\x00")
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				buf, _ = c.Get(k)
+				if !bytes.Equal(buf, v) {
+					panic(fmt.Errorf("BUG: invalid value obtained; got %q; want %q", buf, v))
+				}
+			}
+		}
+	})
+}
+
+//fastcache
+func BenchmarkFastcacheGet(b *testing.B) {
+	const items = 1 << 16
+	c := fastcache.New(12 * items)
+	defer c.Reset()
+	k := []byte("\x00\x00\x00\x00")
+	v := []byte("xyza")
+	for i := 0; i < items; i++ {
+		k[0]++
+		if k[0] == 0 {
+			k[1]++
+		}
+		c.Set(k, v)
+	}
+
+	b.ReportAllocs()
+	b.SetBytes(items)
+	b.RunParallel(func(pb *testing.PB) {
+		var buf []byte
+		k := []byte("\x00\x00\x00\x00")
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				buf = c.Get(buf[:0], k)
+				if !bytes.Equal(buf, v) {
+					panic(fmt.Errorf("BUG: invalid value obtained; got %q; want %q", buf, v))
+				}
+			}
+		}
+	})
+}
+
+//std
+func BenchmarkStdSyncMapGet(b *testing.B) {
+	const items = 1 << 16
+	m := sync.Map{}
+	k := []byte("\x00\x00\x00\x00")
+	v := "xyza"
+	for i := 0; i < items; i++ {
+		k[0]++
+		if k[0] == 0 {
+			k[1]++
+		}
+		m.Store(string(k), v)
+	}
+
+	b.ReportAllocs()
+	b.SetBytes(items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				vv, ok := m.Load(string(k))
+				if !ok || vv.(string) != string(v) {
+					panic(fmt.Errorf("BUG: unexpected value; got %q; want %q", vv, v))
+				}
+			}
+		}
+	})
+}
+
+//Std
+func BenchmarkStdMapGet(b *testing.B) {
+	const items = 1 << 16
+	m := make(map[string][]byte)
+	k := []byte("\x00\x00\x00\x00")
+	v := []byte("xyza")
+	for i := 0; i < items; i++ {
+		k[0]++
+		if k[0] == 0 {
+			k[1]++
+		}
+		m[string(k)] = v
+	}
+
+	var mu sync.RWMutex
+	b.ReportAllocs()
+	b.SetBytes(items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				mu.RLock()
+				vv := m[string(k)]
+				mu.RUnlock()
+				if string(vv) != string(v) {
+					panic(fmt.Errorf("BUG: unexpected value; got %q; want %q", vv, v))
+				}
+			}
+		}
+	})
+}
+
+//noGCMap
+func BenchmarkSetGet(b *testing.B) {
+	const items = 1 << 16
+	c := New()
+	b.ReportAllocs()
+	b.SetBytes(2 * items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		v := []byte("xyza")
+		var buf []byte
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				c.Set(k, v)
+			}
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				buf, _ = c.Get(k)
+				if !bytes.Equal(buf, v) {
+					panic(fmt.Errorf("BUG: invalid value obtained; got %q; want %q", buf, v))
+				}
+			}
+		}
+	})
+}
+
+//Fastcache
+func BenchmarkFastcacheSetGet(b *testing.B) {
+	const items = 1 << 16
+	c := fastcache.New(12 * items)
+	defer c.Reset()
+	b.ReportAllocs()
+	b.SetBytes(2 * items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		v := []byte("xyza")
+		var buf []byte
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				c.Set(k, v)
+			}
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				buf = c.Get(buf[:0], k)
+				if !bytes.Equal(buf, v) {
+					panic(fmt.Errorf("BUG: invalid value obtained; got %q; want %q", buf, v))
+				}
+			}
+		}
+	})
+}
+
+//Std
+func BenchmarkStdMapSetGet(b *testing.B) {
+	const items = 1 << 16
+	m := make(map[string][]byte)
+	var mu sync.RWMutex
+	b.ReportAllocs()
+	b.SetBytes(2 * items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		v := []byte("xyza")
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				mu.Lock()
+				m[string(k)] = v
+				mu.Unlock()
+			}
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				mu.RLock()
+				vv := m[string(k)]
+				mu.RUnlock()
+				if string(vv) != string(v) {
+					panic(fmt.Errorf("BUG: unexpected value; got %q; want %q", vv, v))
+				}
+			}
+		}
+	})
+}
+
+//StdSyncMap
+func BenchmarkSyncMapSetGet(b *testing.B) {
+	const items = 1 << 16
+	m := sync.Map{}
+	b.ReportAllocs()
+	b.SetBytes(2 * items)
+	b.RunParallel(func(pb *testing.PB) {
+		k := []byte("\x00\x00\x00\x00")
+		v := "xyza"
+		for pb.Next() {
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				m.Store(string(k), v)
+			}
+			for i := 0; i < items; i++ {
+				k[0]++
+				if k[0] == 0 {
+					k[1]++
+				}
+				vv, ok := m.Load(string(k))
+				if !ok || vv.(string) != string(v) {
+					panic(fmt.Errorf("BUG: unexpected value; got %q; want %q", vv, v))
+				}
+			}
+		}
+	})
+}
